2d386d8bfaf88bcdfb9a3dd1d11008c8135d342d
Ronald Boda <7385395+skipman@users.noreply.github.com>
2022-07-22 11:58:45 +0200

    Use Haste to generate HelloWorldModule wildcard

--- src/Module/HelloWorldModule.php
+++ src/Module/HelloWorldModule.php
@@ -2,8 +2,7 @@
 
 namespace Skipman\ContaoHelloWorldBundle\Module;
 
-use Skipman\ContaoHelloWorldBundle\Library\MessageGenerator;
-class HelloWorldModule extends \Module
+class HelloWorldModule extends \Haste\Frontend\AbstractFrontendModule
 {
     /**
      * @var string
@@ -11,33 +10,11 @@ class HelloWorldModule extends \Module
     protected $strTemplate = 'mod_helloWorld';
 
     /**
-     * Displays a wildcard in the back end.
-     *
-     * @return string
-     */
-    public function generate()
-    {
-        if (TL_MODE == 'BE') {
-            $template = new \BackendTemplate('be_wildcard');
-
-            $template->wildcard = '### '.utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['helloWorld'][0]).' ###';
-            $template->title = $this->headline;
-            $template->id = $this->id;
-            $template->link = $this->name;
-            $template->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id='.$this->id;
-
-            return $template->parse();
-        }
-
-        return parent::generate();
-    }
-
-    /**
      * Generates the module.
      */
     protected function compile()
     {
-        $messageGenerator = \Contao\System::getContainer()->get(MessageGenerator::class);
+        $messageGenerator = \Contao\System::getContainer()->get('acme.contao_hello_world_bundle.message_generator');
 
         $message = $messageGenerator->sayHelloTo('World');
 
